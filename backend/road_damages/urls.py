0463040d069d6f0f38ec5f61a0712298831561e8
mkumaszka <martyna.kumaszka18@gmail.com>
2018-10-27 15:07:18 +0200

    Removed unused urls

--- backend/road_damages/urls.py
+++ backend/road_damages/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 app_name = 'road_damages'
 urlpatterns = [
-    path('upload_video/', views.upload_video, name='upload_video'),
-    path('upload_multiple_images/', views.upload_multiple_images, name='upload_multiple_images'),
     path('file_upload/', views.file_upload, name='file_upload'),
     path('damages/', views.ListDamages.as_view(), name='damages'),
     path('damages/<int:pk>/', views.DamageDetail.as_view(), name='damage_detail'),
